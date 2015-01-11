62a24c7ee9dac50791933582592ff9450cefe88b
Raphaël Barrois <raphael.barrois@polytechnique.org>
2015-01-11 20:55:18 +0100

    setup.py: Always read as utf-8

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2011-2013 Raphaël Barrois
+# Copyright (c) 2011-2015 Raphaël Barrois
 
+import codecs
 import os
 import re
 import sys
@@ -14,8 +15,8 @@ root_dir = os.path.abspath(os.path.dirname(__file__))
 def get_version(package_name):
     version_re = re.compile(r"^__version__ = [\"']([\w_.-]+)[\"']$")
     package_components = package_name.split('.')
-    path_components = package_components + ['__init__.py']
-    with open(os.path.join(root_dir, *path_components)) as f:
+    init_path = os.path.join(root_dir, *(package_components + ['__init__.py']))
+    with codecs.open(init_path, 'r', 'utf-8') as f:
         for line in f:
             match = version_re.match(line[:-1])
             if match:
@@ -24,7 +25,7 @@ def get_version(package_name):
 
 
 def parse_requirements(requirements_file):
-    with open(requirements_file, 'r') as f:
+    with codecs.open(requirements_file, 'r', 'utf-8') as f:
         return [line for line in f if line.strip() and not line.startswith('#')]
 
 
