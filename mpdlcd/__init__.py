8a02cb3a0f708f48f9ed30499a4eb88e52714493
Raphaël Barrois <raphael.barrois@polytechnique.org>
2015-05-11 23:36:54 +0200

    Version bump to 0.4.2

--- mpdlcd/__init__.py
+++ mpdlcd/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2011-2013 Raphaël Barrois
 
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 __author__ = u"Raphaël Barrois <raphael.barrois+mpdlcd@polytechnique.org>"
